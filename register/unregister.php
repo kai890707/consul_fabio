0a2c530fba7d021dc6cab2bfd75c53d38a4cc471
蔡銘凱 <z85385637@gmail.com>
2023-10-04 16:59:03 +0800

    Dev : append unregister

--- register/unregister.php
+++ register/unregister.php
@@ -1,5 +1,5 @@
 <?
-
+require_once './function.php';
 http_request('http://10.1.1.7:8500/v1/agent/service/deregister/order_service1',"PUT");
 http_request('http://10.1.1.7:8500/v1/agent/service/deregister/product_service1',"PUT");
 http_request('http://10.1.1.7:8500/v1/agent/service/deregister/product_service2',"PUT");
